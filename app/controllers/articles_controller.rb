4c8eea142cb84add2ec704609b85017017f9350c
Kelsey James <kelseybjames@gmail.com>
2016-01-22 09:14:24 -0800

    Starting on jompstart blogger project:

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+end
