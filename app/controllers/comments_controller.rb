7bf1252854cccc981fc0a719e07fa49c7d31a54d
Kelsey James <kelseybjames@gmail.com>
2016-01-22 16:02:47 -0800

    Revert "Added sorcery authentication"
    This reverts commit ddded253be64092aaa5611bf4b18fcbead76d674.
    
    PostgreSQL conversion failed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   include CommentsHelper
-  before_filter :require_login, except: [:create]
   
   def create
     @comment = Comment.new(comment_params)
